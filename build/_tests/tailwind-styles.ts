b39d8044ae4a43747049e32a2e6e6f0358de820a
Tim Bendt <timbendt@granular.ag>
2023-06-09 14:01:50 -0600

    fix: react template type error T onclick

--- build/_tests/tailwind-styles.ts
+++ build/_tests/tailwind-styles.ts
@@ -3719,7 +3719,7 @@ get contentH2() { return this.add("content h2"); }
 export const $$ = (selector?: string): BaseStyles => {
   return new BaseStyles('' + selector || '')
 }
-export function $onClick(fn: React.MouseEventHandler<T>) {
+export function $onClick<T>(fn: React.MouseEventHandler<T>) {
   return (child: BaseStyles, ...children: BaseStyleArgs<T>) => {
     const firstChild = children[0]
     const firstChildIsProps = Boolean(
